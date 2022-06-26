48530211fef4b769b7cfed694abf1e40b47279c5
GabrielFrigo <gabriel.frigo4@gmail.com>
2022-06-26 13:23:23 -0300

    up

--- rbin.sh
+++
@@ -1,7 +0,0 @@
-if test "`whoami`" != "root" ; then
-	echo "You must be logged in as root to build (for loopback mounting)"
-	echo "Enter 'su' or 'sudo bash' to switch to root"
-	exit
-fi
-
-rm -r BinOS
