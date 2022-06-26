4ae39f81b8579a675fb96443cfe642ea74e74160
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2022-06-26 13:05:47 -0300

    Update

--- run.sh
+++ run.sh
@@ -1,44 +1,7 @@
 #!/bin/sh
 
-if [ ! -e BinOS ]
-then
-	mkdir BinOS
-fi
+sudo ./build.sh
 
-if test "`whoami`" != "root" ; then
-	echo "You must be logged in as root to build (for loopback mounting)"
-	echo "Enter 'su' or 'sudo bash' to switch to root"
-	exit
-fi
-
-if [ ! -e BinOS/FrigoOS.flp ]
-then
-	mkdosfs -C BinOS/FrigoOS.flp 1440 || exit
-fi
-
-nasm -O0 -w+orphan-labels -f bin -o  BinOS/bootloader.bin src/Bootloader.asm || exit
-
-nasm -O0 -w+orphan-labels -f bin -o BinOS/kernel.bin src/Kernel.asm  || exit
-
-dd status=noxfer conv=notrunc if=BinOS/bootloader.bin of=BinOS/FrigoOS.flp || exit
-
-cd BinOS
-
-rm -rf tmp-loop
-
-mkdir tmp-loop && mount -o loop -t vfat FrigoOS.flp tmp-loop && cp kernel.bin tmp-loop/
-
-sleep 0.2
-
-umount tmp-loop || exit
-
-rm -rf tmp-loop
-
-rm -f FrigoOS.iso
-mkisofs -quiet -V 'FRIGOOS' -input-charset iso8859-1 -o FrigoOS.iso -b FrigoOS.flp ./ || exit
-
-sleep 0.2
-
-qemu-system-i386 -fda FrigoOS.flp
+qemu-system-i386 -fda BinOS/FrigoOS.flp
 
 echo '>>> Done!'
