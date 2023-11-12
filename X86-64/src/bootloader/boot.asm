a66e329799c5ab42a4a87b6333b05c4f0329ab0f
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2023-11-12 13:12:25 -0300

    Update BOOT

--- X86-64/src/bootloader/boot.asm
+++ X86-64/src/bootloader/boot.asm
@@ -1,23 +1,18 @@
-[org 0x7c00]
-    xor ax, ax      ; Clear ax
-    mov ds, ax      ; Set data segment to 0
-    mov es, ax      ; Set extra segment to 0
-    mov si, msg     ; Set si to point to the message
-    call print      ; Call print function
-    jmp $           ; Infinite loop
+bits 16 ; tell NASM this is 16 bit code
+org 0x7c00 ; tell NASM to start outputting stuff at offset 0x7c00
+boot:
+    mov si,hello ; point si register to hello label memory location
+    mov ah,0x0e ; 0x0e means 'Write Character in TTY mode'
+.loop:
+    lodsb
+    or al,al ; is al == 0 ?
+    jz halt  ; if (al == 0) jump to halt label
+    int 0x10 ; runs BIOS interrupt 0x10 - Video Services
+    jmp .loop
+halt:
+    cli ; clear interrupt flag
+    hlt ; halt execution
+hello: db "Hello world!",0
 
-print:
-    lodsb           ; Load character from si into al
-    or al, al       ; Check for null terminator
-    jz done         ; If null terminator, jump to done
-    mov ah, 0x0e    ; Set teletype mode
-    int 0x10        ; Call BIOS interrupt to print character
-    jmp print       ; Loop back to print next character
-done:
-    ret
-
-msg:
-    db "Hello, World!", 0 ; Null-terminated message string
-
-times 510-($-$$) db 0 ; Fill the rest of the sector with 0s
-dw 0xaa55 ; Boot signature
+times 510 - ($-$$) db 0 ; pad remaining 510 bytes with zeroes
+dw 0xaa55 ; magic bootloader magic - marks this 512 byte sector bootable!
